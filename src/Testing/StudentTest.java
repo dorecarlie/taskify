7a38f361d3ec541184effd30a22961866291b5c7
Karyme-Raudales <raudalek2@newpaltz.edu>
2026-04-29 13:32:20 -0700

    added testing class
    added testing class

---
+++ src/Testing/StudentTest.java
@@ -0,0 +1,36 @@
+package Testing;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class StudentTest {
+ 
+    @Test
+    public void testCreateTask(){
+        //test to check task is created
+
+        //normal case
+
+        //edge case
+
+        //error case
+
+    }
+
+        @Test
+    public void testUpdateTask(){
+
+
+    }
+
+        @Test
+    public void testDeleteTask(){
+    
+
+
+    }
+
+
+
+
+
+}
