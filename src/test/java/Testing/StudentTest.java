77aefea16ed673ba067045570a10fbc843fa656d
Karyme-Raudales <raudalek2@newpaltz.edu>
2026-04-30 17:07:27 -0700

    added a new test method

--- src/test/java/Testing/StudentTest.java
+++ src/test/java/Testing/StudentTest.java
@@ -30,11 +30,32 @@ public class StudentTest {
         assertEquals(deadline, manager.getTasks().get(0).getDeadline()); // correct deadline
     }
 
-    //     @Test
-    // public void testUpdateTask(){
+        @Test
+    public void testUpdateTask(){
+        // arrange
+        ConcreteTaskManager manager = new ConcreteTaskManager();
+        Student student = new Student();
+        student.setTaskManager(manager);
 
+        LocalDateTime deadline1 = LocalDateTime.of(2026, 5, 1, 12, 0);
+        LocalDateTime deadline2 = LocalDateTime.of(2026, 6, 1, 12, 0);
 
-    // }
+        Task originalTask = new Task(1, "Old Task", deadline1, 1);
+        Task updatedTask = new Task(1, "New Task", deadline2, 2);
+
+        // add original task first
+        student.createTask(originalTask);
+
+        //update it
+        student.updateTask(1, updatedTask);
+
+        // assert
+        assertEquals(1, manager.getTasks().size()); // still 1 task
+        assertEquals("New Task", manager.getTasks().get(0).getTitle()); // replaced
+        assertEquals(deadline2, manager.getTasks().get(0).getDeadline()); // updated deadline
+        assertEquals(2, manager.getTasks().get(0).getPriority()); // updated priority
+
+    }
 
     //     @Test
     // public void testDeleteTask(){
