9abb74b242c4cf8c79c70cc91b98dd29427212f4
dorecarlie <dorecarlie@gmail.com>
2026-04-30 21:13:58 -0400

    Update the update method for List<Task>

--- src/main/java/taskify/StudentPlanner/CalendarView.java
+++ src/main/java/taskify/StudentPlanner/CalendarView.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class CalendarView implements Observer{
     private Map<Date, List<Task>> calendarView = new HashMap<>();
 
-    public void update(Task task){
+    public void update(List<Task> task){
         System.out.println("Updating Calender for " + task.getTitle() + "...");
         displayCalendar();
         refreshView();
